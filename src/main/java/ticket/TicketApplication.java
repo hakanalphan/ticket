b69180e2117f41a6fdcd0212280c29c8eb0672e8
hakan alphan <hakanalphan63@gmail.com>
2023-07-25 11:57:11 +0300

    21 temmuz

--- src/main/java/ticket/TicketApplication.java
+++ src/main/java/ticket/TicketApplication.java
@@ -3,17 +3,13 @@ package ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableSwagger2WebMvc // Swagger'ı etkinleştiren anotasyon
 public class TicketApplication {
 
 	public static void main(String[] args) {
@@ -25,19 +21,6 @@ public class TicketApplication {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("ticket.controller"))
-				.paths(PathSelectors.any())
-				.build()
-				.apiInfo(apiInfo());
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Ticket API Documentation")
-				.description("API documentation for Ticket Application")
-				.version("1.0.0")
 				.build();
 	}
 }
-
-
-
